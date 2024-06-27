a3db4b9e40c297265b8cf6966c18826e97a44c2c
lastsxxnst <167818521+lastsxxnst@users.noreply.github.com>
2024-06-28 01:07:09 +0300

    Add files via upload

---
+++ fourhexercise.py
@@ -0,0 +1,40 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Generate keys (private and public)
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+public_key = private_key.public_key()
+
+# Example message
+original_message = b"Information Systems"
+
+# Create a digital signature
+signature = private_key.sign(
+    original_message,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)
+
+# Verify the digital signature
+try:
+    public_key.verify(
+        signature,
+        original_message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    print("Signature is valid. The message has not been altered.")
+except Exception:
+    print("Invalid signature. The message was modified or the key is incorrect.")
